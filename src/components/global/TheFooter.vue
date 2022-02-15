a43cb90642f30eae06c03321bdb7ad28762f92b1
moh1434 <moh1434.ma@gmail.com>
2022-02-16 00:57:34 +0300

    update Copyrights

--- src/components/global/TheFooter.vue
+++ src/components/global/TheFooter.vue
@@ -265,11 +265,11 @@
       </div>
     </div>
     <div class="text-center p-6 bg-gray-200">
-      <span>© 2021 Copyright:</span>
+      <span>© 2022 Copyright:</span>
       <a
         class="text-gray-600 font-semibold"
         href="https://tailwind-elements.com/"
-        >Tailwind Elements</a
+        >Maslaha</a
       >
     </div>
   </footer>
