e0620971b2008426f8697e4e2c25bdd463f3e834
moh1434 <moh1434.ma@gmail.com>
2022-02-16 00:58:48 +0300

    extract icons data

---
+++ src/types/icons.ts
@@ -0,0 +1,19 @@
+type GlobalSvgAttributesI = Record<
+  'focusable' | 'data-prefix' | 'role' | 'xmlns',
+  string
+> & { 'aria-hidden': boolean };
+type GlobalPathAttributesI = {
+  fill?: string;
+};
+export type svgI = {
+  svgAttributes: {
+    'data-icon': string;
+    viewBox: string;
+  } & { [k in string]: string };
+  pathAttributes: { d: string } & { [k in string]: string };
+};
+export type socialIconsI<T extends svgI> = {
+  GlobalSvgAttributes: GlobalSvgAttributesI;
+  GlobalPathAttributes: GlobalPathAttributesI;
+  icons: T[];
+};
