7de26e40251545309a778686336244b5889d2798
moh1434 <moh1434.ma@gmail.com>
2022-02-15 20:35:54 +0300

    rename image-parent to background-section

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <section class="image-parent relative">
+  <section class="background-section relative">
     <div class="absolute relative-center">
       <h1>Complete your projects online easily and safely</h1>
       <h2>Get freelancers to complete your projects</h2>
@@ -10,39 +10,40 @@
 </template>
 
 <style>
-.image-parent {
+/* Start background-section */
+.background-section {
   background: url('@/assets/imgs/img1.jpg') black;
   background-size: cover;
   background-repeat: no-repeat;
-  height: 85vh;
+  height: 90vh;
   color: black;
   text-align: center;
 }
-.image-parent h1 {
+.background-section h1 {
   @apply text-2xl font-bold;
 }
-.image-parent h2 {
+.background-section h2 {
   @apply text-lg mt-2;
 }
-.image-parent > div {
+.background-section > div {
   @apply p-5 bg-gray-200 bg-opacity-90 rounded-3xl;
 }
 
 .relative-center {
-  top: 70%;
+  top: 60%;
   left: 0;
   right: 0;
   @apply mx-12;
   transform: translateY(-50%);
 }
 @media screen(md) {
-  .image-parent h1 {
+  .background-section h1 {
     @apply text-3xl;
   }
-  .image-parent h2 {
+  .background-section h2 {
     @apply text-2xl mt-6;
   }
-  .image-parent > div {
+  .background-section > div {
     @apply p-9;
   }
   .relative-center {
@@ -51,10 +52,10 @@
     @apply mx-auto;
   }
 }
-
 @media screen(2xl) {
-  .image-parent {
+  .background-section {
     background-position: 0 -120px;
   }
 }
+/* End background-section */
 </style>
