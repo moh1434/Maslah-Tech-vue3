de027c20bb269752d547b2e5b6872df7135af37a
moh1434 <moh1434.ma@gmail.com>
2022-02-16 02:10:00 +0300

    extract GlobalPathAttributes to other file

---
+++ src/constants/iconsGlobalAttributes.ts
@@ -0,0 +1,8 @@
+export const GlobalSvgAttributes = Object.freeze({
+  'aria-hidden': true,
+  focusable: 'false',
+  'data-prefix': 'fab',
+  role: 'img',
+  xmlns: 'http://www.w3.org/2000/svg',
+});
+export const GlobalPathAttributes = Object.freeze({ fill: 'currentColor' });
