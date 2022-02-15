8173c878fae9b02596cbc1f5f37695c6f1c5f21d
moh1434 <moh1434.ma@gmail.com>
2022-02-15 20:36:47 +0300

    use WhyMaslahaCard

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,54 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import WhyMaslahaCard from '@/components/WhyMaslahaCard.vue';
+import { ref } from 'vue';
+const WhyMaslahaData = ref<{ svgId: string; title: string; p: string }[]>([
+  {
+    svgId: '#lightbulb',
+    title: 'Complete your creative idea',
+    p: 'Choose a professional freelancer to complete your work effortlessly',
+  },
+  {
+    svgId: '#thumbs-up',
+    title: 'Hire the best for you',
+    p: 'Visit freelancers profiles, see their past work and their price, then hire the best',
+  },
+  {
+    svgId: '#money-bill-alt',
+    title: 'Pay safely',
+    p: 'Pay for work only when it has been completed and you are satisfied with the quality.',
+  },
+  {
+    svgId: '#handshake',
+    title: 'We are here to help',
+    p: 'Our talented team of recruiters can help you find the best freelancer for the job.',
+  },
+]);
+</script>
 
 <template>
   <section class="background-section relative">
     <div class="absolute relative-center">
       <h1>Complete your projects online easily and safely</h1>
       <h2>Get freelancers to complete your projects</h2>
+    </div>
+  </section>
+  <section class="bg-cyan-600 text-center py-14">
+    <div class="container mx-auto flex flex-wrap justify-evenly">
+      <WhyMaslahaCard
+        v-for="(data, index) in WhyMaslahaData"
+        :key="index"
+        class="md:w-1/2 2xl:w-1/4"
+      >
+        <template #icon>
+          <svg><use :xlink:href="data.svgId" /></svg>
+        </template>
+        <template #title-text>{{ data.title }}</template>
+        <template #p>
+          <p class="xl:max-w-md xl:mx-auto">
+            {{ data.p }}
+          </p>
+        </template>
+      </WhyMaslahaCard>
     </div>
   </section>
 </template>
