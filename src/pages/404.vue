22c8722309aade0eee6ca14583ef48c4b8f18a49
moh1434 <moh1434.ma@gmail.com>
2022-02-16 02:08:30 +0300

    create 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex h-full">
+    <p class="m-auto text-3xl font-bold">Cooming Soon</p>
+  </div>
+</template>
+
+<style></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,13 @@ import Index from '@/pages/index.vue';
 // console.log('meta.env.MODE=', import.meta.env.MODE);
 
 const history = createWebHistory();
-const routes = [{ path: '/', name: 'index', component: Index }];
+const routes = [
+  { path: '/', name: 'index', component: Index },
+  {
+    path: '/:pathMatch(.*)*',
+    name: '404',
+    component: () => import('@/pages/404.vue'),
+  },
+];
 const router = createRouter({ history, routes });
 export default router;
