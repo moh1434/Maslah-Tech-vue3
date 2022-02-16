8b71e119d5a42614c72e0a023c3fc233958df5e7
moh1434 <moh1434.ma@gmail.com>
2022-02-16 03:33:00 +0300

    add index page

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex h-full">
-    <p class="m-auto text-3xl font-bold">Cooming Soon</p>
+  <div class="flex flex-col h-full text-center">
+    <div class="m-auto">
+      <p class="text-3xl font-bold mb-3">Cooming Soon</p>
+      <router-link
+        :to="{ name: 'index' }"
+        class="text-2xl underline text-blue-700 hover:text-blue-500"
+        >Home page</router-link
+      >
+    </div>
   </div>
 </template>
 
