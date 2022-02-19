fc40bb5859299986db6e11d7d28ea16b74be6992
moh1434 <moh1434.ma@gmail.com>
2022-02-19 23:45:24 +0300

    Translate 404.vue page

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,13 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n';
+const { t } = useI18n();
+</script>
 
 <template>
   <div class="flex flex-col h-full text-center">
     <div class="m-auto">
-      <p class="text-3xl font-bold mb-3">Cooming Soon</p>
+      <p class="text-3xl font-bold mb-3">{{ t('404.Cooming_soon') }}</p>
       <router-link
         :to="{ name: 'index' }"
         class="text-2xl underline text-blue-700 hover:text-blue-500"
-        >Home page</router-link
+        >{{ t('Home') + ' ' + t('page') }}</router-link
       >
     </div>
   </div>
