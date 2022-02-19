c72dcb356ef72e17333b1ac3ea7a4d0096e11de3
moh1434 <moh1434.ma@gmail.com>
2022-02-19 23:09:03 +0300

    add i18n and locals files

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import vueI18n from '@intlify/vite-plugin-vue-i18n';
 const path = require('path');
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vueI18n({
+      // you need to set i18n resource including paths !
+      include: path.resolve(__dirname, './src/locales/**'),
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
