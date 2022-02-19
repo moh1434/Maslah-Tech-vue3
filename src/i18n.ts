d4ebad1b5df6215558fdf23063f3994b880f5571
moh1434 <moh1434.ma@gmail.com>
2022-02-19 23:07:00 +0300

    extract i18n from main.ts to use it outside vueComoponents

---
+++ src/i18n.ts
@@ -0,0 +1,18 @@
+//Start i18n
+import { createI18n } from 'vue-i18n';
+import ar from '@/locales/ar.json';
+import en from '@/locales/en.json';
+
+const currentLocale: 'en' | 'ar' =
+  localStorage.getItem('currentLocale') == 'ar' ? 'ar' : 'en';
+export default createI18n({
+  legacy: false, // use composition api
+  locale: currentLocale, // set locale
+  availableLocales: ['ar', 'en'],
+  fallbackLocale: 'en', // set fallback locale
+  messages: {
+    en,
+    ar,
+  },
+});
+//End i18n
