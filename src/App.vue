f143a8a29bfdf27c5b1d31693784ed1099e7f051
moh1434 <moh1434.ma@gmail.com>
2022-02-21 23:36:49 +0300

    change direction in arabic lang

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import { computed } from 'vue';
 import TheHeader from '@/components/global/TheHeader.vue';
 import TheFooter from '@/components/global/TheFooter.vue';
+
+import { useI18n } from 'vue-i18n';
+
+const { locale } = useI18n();
+
+const direction = computed(() => (locale.value == 'ar' ? 'rtl' : 'ltr'));
 </script>
 
 <template>
@@ -33,6 +40,17 @@ body {
 /* #__nuxt, */
 main {
   flex-grow: 1;
+}
+
+.rtl {
+  direction: rtl;
+}
+
+.ltr {
+  direction: ltr;
+}
+input {
+  direction: v-bind(direction);
 }
 </style>
 <style src="@/assets/css/tailwind.css"></style>
