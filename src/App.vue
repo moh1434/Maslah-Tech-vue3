0eccaf836541c7208e9f8627d0464ffa89ab8553
moh1434 <moh1434.ma@gmail.com>
2022-02-21 23:55:32 +0300

    Refactore langDirection

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import { computed } from 'vue';
 import TheHeader from '@/components/global/TheHeader.vue';
 import TheFooter from '@/components/global/TheFooter.vue';
 
-import { useI18n } from 'vue-i18n';
-
-const { locale } = useI18n();
-
-const direction = computed(() => (locale.value == 'ar' ? 'rtl' : 'ltr'));
+import direction from '@/helpers/langDirection';
 </script>
 
 <template>
---
+++ src/helpers/langDirection.ts
@@ -0,0 +1,8 @@
+import { computed, ComputedRef } from 'vue';
+import i18n from '@/i18n';
+const { locale } = i18n.global;
+
+const cssDirection: ComputedRef<'rtl' | 'ltr'> = computed(() =>
+  locale.value == 'ar' ? 'rtl' : 'ltr'
+);
+export default cssDirection;
