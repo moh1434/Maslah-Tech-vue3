5903c96c90bf1fe32ab7e99b85b35f2a5de714bd
moh1434 <moh1434.ma@gmail.com>
2022-03-01 02:44:13 +0300

    refreshLocalUserData when user register

--- src/helpers/Auth/localAuth.ts
+++ src/helpers/Auth/localAuth.ts
@@ -1,12 +1,12 @@
 import { ref } from 'vue';
 
-const clearLocalUser = {
-  id: undefined,
-  name: undefined,
-  email: undefined,
-  picture: undefined,
-  token: null,
-};
+// const clearLocalUser = {
+//   id: undefined,
+//   name: undefined,
+//   email: undefined,
+//   picture: undefined,
+//   token: null,
+// };
 
 const localUser = ref<{
   id: string | undefined;
@@ -21,21 +21,32 @@ const localUser = ref<{
   picture: undefined,
   token: null,
 });
-const userData = localStorage.getItem('userData');
-if (userData) {
-  try {
-    localUser.value = { ...JSON.parse(userData) };
-  } catch (e) {
-    alert(e);
+const localUserKeys = Object.keys(localUser.value) as Array<
+  keyof typeof localUser.value
+>;
+function refreshLocalUserData() {
+  const userData = localStorage.getItem('userData');
+  if (userData) {
+    const newLocalUser = JSON.parse(userData);
+    try {
+      localUserKeys.map((key) => {
+        if (Object.prototype.hasOwnProperty.call(newLocalUser, key)) {
+          localUser.value[key] = newLocalUser[key];
+        }
+      });
+    } catch (e) {
+      alert(e);
+    }
   }
+  localUser.value.token = localStorage.getItem('token');
 }
-localUser.value.token = localStorage.getItem('token');
 
+refreshLocalUserData();
 const defaultImagePath = './public/imgs/defaultAvatar.png';
 
 function logOut() {
   localStorage.removeItem('token');
   localStorage.removeItem('userData');
-  localUser.value = { ...clearLocalUser };
+  refreshLocalUserData();
 }
-export { localUser, defaultImagePath, logOut };
+export { localUser, defaultImagePath, logOut, refreshLocalUserData };
