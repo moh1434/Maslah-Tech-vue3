2bc1ea7147bddb4a192fb26c6acbfd947db28099
moh1434 <moh1434.ma@gmail.com>
2022-03-02 02:08:08 +0300

    new design

--- src/pages/Categories.vue
+++ src/pages/Categories.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import Category from '@/components/Category.vue';
-import { CategoryI } from '@/types/Categroy';
+import SubCategory from '@/components/SubCategory.vue';
+
 import { fetchCategories } from '@/api/FetchCategories';
 
 import { ref } from 'vue';
 
 import { useI18n } from 'vue-i18n';
 import H1 from '@/components/small/H1.vue';
+
+import { CategoryI } from '@/types/Categroy';
 const { t } = useI18n();
 
 const categories = ref<Array<CategoryI>>([]); //ref<Array<CategoryI>>([]);
@@ -27,13 +30,25 @@ fetchCategories()
 
 <template>
   <div>
-    <div class="flex flex-wrap justify-evenly gap-10 my-12 max-w-7xl mx-auto">
-      <H1 class="w-full mx-7">{{ t('categories.h1') }}</H1>
-      <Category
+    <div class="my-12 max-w-7xl mx-auto">
+      <H1 class="w-full px-7 pb-7">{{ t('categories.h1') }}</H1>
+      <div
+        class="flex flex-wrap justify-center p-2"
         v-for="category in categories"
         :key="category.id"
-        :category="category"
-      ></Category>
+      >
+        <Category :category="category" class="w-full mx-4 mb-4 text-center" />
+        <SubCategory
+          class="sm:w-64 w-1/2"
+          v-for="subCategory in category.children"
+          :key="subCategory.id"
+          :category="subCategory"
+        />
+
+        <hr
+          class="w-4/5 mx-auto my-10 bg-black border rounded-full border-black"
+        />
+      </div>
     </div>
   </div>
 </template>
