3ccb1f0c21242555786434be5ae2b75e65efee4a
moh1434 <moh1434.ma@gmail.com>
2022-03-02 23:43:02 +0300

    better style

--- src/components/small/H1.vue
+++ src/components/small/H1.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1 class="font-medium leading-tight text-3xl capitalize direction">
+  <h1
+    class="font-medium leading-tight text-2xl sm:text-3xl capitalize direction"
+  >
     <slot />
   </h1>
 </template>
