2d07e6d2fdca5857b3dea98b2b5a235dbfda5524
moh1434 <moh1434.ma@gmail.com>
2022-03-08 12:28:31 +0300

    add vueUse to use useFetch

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,5 +1,14 @@
+import { createFetch } from '@vueuse/core';
 import axios from 'axios';
 
 export const api = axios.create({
   baseURL: `http://192.168.3.67:3001/v1/`,
+});
+
+export const useMyFetch = createFetch({
+  baseUrl: 'http://192.168.3.67:3001/v1/',
+  options: {},
+  fetchOptions: {
+    mode: 'cors',
+  },
 });
