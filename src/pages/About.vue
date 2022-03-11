ae0d3471d8194a7f740d2e14cc631afae16ce4a7
moh1434 <moh1434.ma@gmail.com>
2022-03-11 19:23:18 +0300

    complete about-us page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,7 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import H1 from '@/components//small/H1.vue';
+</script>
 
 <template>
-  <div>About</div>
+  <div class="direction max-w-3xl mx-auto my-6 sm:my-10">
+    <img
+      class="mx-auto mb-4 sm:mb-8"
+      src="imgs/logo1_400X200.png"
+      alt="maslahTech image"
+    />
+    <H1 class="mx-4 mb-4">نبذة عن MaslahTech</H1>
+    <p class="mx-2 sm:text-lg">
+      مصلحتك هو السوق العراقي الأول لبيع وشراء الخدمات المصغرة، يجمع مصلحتك بين
+      الشباب العراقي المستعد لتقديم الخدمات وبين فئة المشترين المستعدين لشراء
+      هذه الخدمات، وبذلك يوفر دخلاً مناسباً للشباب العراقي وخدمات مميزة بسعر
+      اقتصادي للأفراد والشركات الناشئة. أطلق مصلحتك في عام 2022 كمشروع تخرج
+      لطلاب في قسم هندسة الحاسوب في الجامعة التكنولوجية.
+    </p>
+  </div>
 </template>
 
 <style></style>
