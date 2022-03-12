061fd5788e87b594f382966dfc985f204844de33
moh1434 <moh1434.ma@gmail.com>
2022-03-12 12:56:55 +0300

    add profileI type

---
+++ src/types/ProfileI.ts
@@ -0,0 +1,10 @@
+export type profileI = {
+  id: number;
+  images: string[];
+  description: string;
+  title: string;
+  link: string;
+  createdAt: string;
+  updatedAt: string;
+  userId: string;
+};
