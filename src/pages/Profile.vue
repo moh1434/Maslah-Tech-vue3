d2a559380cc6911eaba37dc097ed2f2739c70dcf
moh1434 <moh1434.ma@gmail.com>
2022-03-12 12:57:12 +0300

    display profiles

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -3,9 +3,15 @@ import { useMyFetch } from '@/api/axios';
 import { fetchUser } from '@/api/fetchUser';
 
 import { userI } from '@/types/UserI';
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { onBeforeRouteUpdate, useRoute } from 'vue-router';
+import { profileI } from '../types/ProfileI';
+import H1 from '@/components/small/H1.vue';
+
+// @ts-ignore
+import { Carousel, Navigation, Slide } from 'vue3-carousel';
+import 'vue3-carousel/dist/carousel.css';
 const route = useRoute();
 const userSkills = ref<Array<string>>([]);
 //start helpers
@@ -35,11 +41,27 @@ function loadTheUser() {
     }
   });
 }
+const profiles = ref<Array<profileI>>([]);
+async function loadUserProfiles() {
+  const { error, data } = await useMyFetch<{
+    data: { data: Array<profileI> };
+  }>(`user/portfilo/${route.params.userId}`).json();
+
+  if (error.value) {
+    alert(error.value);
+  }
+
+  console.log(data.value);
+  if (data.value) {
+    profiles.value = data.value.data;
+  }
+}
 //end helpers
 const user = ref<userI>();
 async function loadThePage() {
   loadTheUser();
   loadUserSkills();
+  loadUserProfiles();
 }
 loadThePage();
 
@@ -50,7 +72,15 @@ onBeforeRouteUpdate((to, from, next) => {
   });
 });
 
-const { t } = useI18n();
+const { t, locale } = useI18n();
+const s3En = ref('');
+watch(
+  locale,
+  (newLocale) => {
+    s3En.value = newLocale == 'en' ? "'s" : '';
+  },
+  { immediate: true }
+);
 </script>
 
 <template>
@@ -80,6 +110,74 @@ const { t } = useI18n();
     </template>
     <div v-else class="text-center">{{ t('no_user_data_yet') }}...</div>
   </section>
+  <section class="my-8 direction lg:m-16">
+    <H1
+      v-if="user"
+      class="m-4 sm:m-6 ltr-i"
+      :class="{ 'text-right': locale == 'ar' }"
+      >{{ user.name.split(' ')[0] + s3En }} {{ t('portfolio') }}</H1
+    >
+    <template v-if="profiles.length">
+      <div class="flex flex-wrap justify-between">
+        <div class="lg:w-1/2" v-for="profile in profiles" :key="profile.id">
+          <div class="m-2 sm:m-4 shadow rounded-md border">
+            <Carousel class="m-4 ltr" :wrap-around="true">
+              <Slide v-for="(image, i) in profile.images" :key="i">
+                <img
+                  alt="image"
+                  :src="image"
+                  class="carousel__item profile-img rounded-md"
+                />
+              </Slide>
+              <template #addons>
+                <Navigation />
+              </template>
+            </Carousel>
+
+            <ul>
+              <li class="flex flex-wrap gap-4 shadow px-4 py-4">
+                <a
+                  class="hover:underline hover:text-blue-600"
+                  :href="profile.link"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <h1 class="text-xl">{{ profile.title }}</h1>
+                </a>
+              </li>
+              <li class="flex gap-4 flex-wrap sm:flex-nowrap shadow px-4 py-4">
+                <span>{{ t('description') }}:</span>
+                <p>{{ profile.description }}</p>
+              </li>
+              <li class="flex flex-wrap gap-4 shadow px-4 py-4">
+                <span>{{ t('preview') }}:</span>
+                <a
+                  class="hover:underline hover:text-blue-600"
+                  :href="profile.link"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  >{{ t('preview_text') }}</a
+                >
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </template>
+    <div v-else class="text-center">{{ t('no_profile_data_yet') }}...</div>
+  </section>
 </template>
 
-<style></style>
+<style>
+@import url('@/assets/css/slider.css');
+
+.profile-img {
+  max-height: 540px;
+
+  object-fit: cover;
+  object-position: 50% 0;
+}
+.ltr-i {
+  direction: ltr !important;
+}
+</style>
