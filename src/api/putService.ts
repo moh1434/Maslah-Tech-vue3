587c2b0d38a93fe7b233f4a7a76579e7b332dfcc
moh1434 <moh1434.ma@gmail.com>
2022-03-14 21:00:02 +0300

    better serviceAPI method

--- src/api/putService.ts
+++ src/api/putService.ts
@@ -22,4 +22,29 @@ async function putService(serviceId: string, payload: Object, token: string) {
   return { response, errors };
 }
 
-export { putService };
+async function serviceAPI(
+  method: 'post' | 'put' | 'delete',
+  url: string,
+  payload: Object,
+  token: string
+) {
+  let response = null;
+  let errors = null;
+  try {
+    response = await api[method]<{ data: serviceI }>(url, payload, {
+      headers: { token: token as string },
+    });
+  } catch (e: any) {
+    if (e?.response?.data?.err) {
+      //The backend puts the errors here :(
+      errors = e.response.data.err;
+    } else {
+      //unknown error
+      errors = e;
+    }
+  }
+
+  return { response, errors };
+}
+
+export { putService, serviceAPI };
