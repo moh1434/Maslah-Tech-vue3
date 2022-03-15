93b96a9ba908d565f83bbc991055c01f0371a8e0
moh1434 <moh1434.ma@gmail.com>
2022-03-15 23:54:38 +0300

    remove createFetch()

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,16 +1,7 @@
-import { createFetch } from '@vueuse/core';
 import axios, { AxiosResponse } from 'axios';
 
 export const api = axios.create({
   baseURL: `http://192.168.3.67:3001/v1/`,
-});
-
-export const useMyFetch = createFetch({
-  baseUrl: 'http://192.168.3.67:3001/v1/',
-  options: {},
-  fetchOptions: {
-    mode: 'cors',
-  },
 });
 
 //new way
