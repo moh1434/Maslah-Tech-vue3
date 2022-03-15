e7476424007eccdce49b99a7352913986631e9dc
moh1434 <moh1434.ma@gmail.com>
2022-03-15 23:10:58 +0300

    remove serviceAPI()

--- src/api/putService.ts
+++ src/api/putService.ts
@@ -1,31 +1,6 @@
 import { serviceI } from '../types/ServiceI';
 import { api } from './axios';
 
-async function serviceAPI<T>(
-  method: 'post' | 'put' | 'delete',
-  url: string,
-  payload: Object,
-  token: string
-) {
-  let response = null;
-  let errors = null;
-  try {
-    response = await api[method]<{ data: T }>(url, payload, {
-      headers: { token: token as string },
-    });
-  } catch (e: any) {
-    if (e?.response?.data?.err) {
-      //The backend puts the errors here :(
-      errors = e.response.data.err;
-    } else {
-      //unknown error
-      errors = e;
-    }
-  }
-
-  return { response, errors };
-}
-
 export async function serviceDelete<T>(url: string, token: string) {
   let response = null;
   let errors = null;
@@ -45,5 +20,3 @@ export async function serviceDelete<T>(url: string, token: string) {
 
   return { response, errors };
 }
-
-export { serviceAPI };
