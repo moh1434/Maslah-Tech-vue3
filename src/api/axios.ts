0a5bf0ae7d4eb7b0920fcfe0b37999a63788d3ec
moh1434 <moh1434.ma@gmail.com>
2022-03-15 20:52:04 +0300

    new way of API

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,5 +1,5 @@
 import { createFetch } from '@vueuse/core';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 export const api = axios.create({
   baseURL: `http://192.168.3.67:3001/v1/`,
@@ -12,3 +12,54 @@ export const useMyFetch = createFetch({
     mode: 'cors',
   },
 });
+
+//new way
+export async function apiWrapper<ServerData>(
+  axiosMethod: () => Promise<
+    AxiosResponse<
+      {
+        data: ServerData;
+      },
+      any
+    >
+  >
+) {
+  let response = null;
+  let errors = null;
+  try {
+    response = await axiosMethod();
+  } catch (e: any) {
+    if (e?.response?.data?.err) {
+      //The backend puts the errors here :(
+      errors = e.response.data.err;
+    } else {
+      //unknown error
+      errors = e;
+    }
+  }
+
+  return { response, errors };
+}
+
+function errorAlerter(errors: any) {
+  if (errors) {
+    try {
+      alert(JSON.stringify(errors));
+    } catch (e) {
+      alert(errors);
+    }
+    return 'alerted';
+  }
+  return; //no errors
+}
+
+// Usage example
+/*
+  apiWrapper<userI>(
+    async () =>
+      await api.get<{ data: userI }>(`/user/${route.params.userId as string}`)
+  ).then(({ response, errors }) => {
+    console.log('res=', response?.data);
+    errorAlerter(errors);
+  });
+ */
